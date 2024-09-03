819a3013e46541df098bc42627163d3a84c36ea6
erdaserhan <erhan209@gmail.com>
2024-09-03 15:01:03 +0200

    homeController update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'title' => '** Homepage **',
         ]);
     }
 }
